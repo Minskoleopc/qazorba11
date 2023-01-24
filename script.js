1c6b222085d4ca0b58b35ae5f36265426a5cd404
Minskoleopc <59231021+Minskoleopc@users.noreply.github.com>
2023-01-24 06:43:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ console.log("Changes")
 console.log("Changes has submitted")
 
 consle.log('chanes on A addedd ddddddddddddddddddddddddd')
+console.log('direct chanes on line 11')
+consle.log('direct changes on line12')
